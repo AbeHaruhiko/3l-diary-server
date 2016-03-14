b5eef362fa503c50a6566b44b7ca83d62868d06d
AbeHaruhiko <caliconography@gmail.com>
2016-03-14 17:14:10 +0900

    add user table.

--- src/main/java/jp/caliconography/SecurityConfig.java
+++ src/main/java/jp/caliconography/SecurityConfig.java
@@ -1,6 +1,10 @@
 package jp.caliconography;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,10 +12,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.anyRequest().authenticated()
 			.and().httpBasic();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+// インメモリで認証するパターン		
+//			.inMemoryAuthentication()
+//			.withUser("user").password("password").roles("USER").and()
+//			.withUser("admin").password("password").roles("USER", "ADMIN");
+			.jdbcAuthentication()
+			.dataSource(dataSource);
 	}
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into users values('test_user', 'test', true);
+insert into authorities values('test_user', 'ROLE_USER');
