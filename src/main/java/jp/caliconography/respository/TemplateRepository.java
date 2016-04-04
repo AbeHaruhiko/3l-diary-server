290bb64f19e31970b178627027787e46ffb1fc56
AbeHaruhiko <h.abe@s2soft.co.jp>
2016-04-04 16:00:54 +0900

    TemplateItemをfetchするように変更。

--- src/main/java/jp/caliconography/respository/TemplateRepository.java
+++ src/main/java/jp/caliconography/respository/TemplateRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.caliconography.domain.Template;
@@ -11,9 +12,8 @@ import jp.caliconography.domain.Template;
 @Repository
 public interface TemplateRepository extends JpaRepository<Template, String> {
 
-	@Query(value ="select distinct x from Template x join fetch x.templateItems order by x.createdAt desc")
-//	@Query("select x from Template x order by x.createdAt desc")
-	List<Template> findAllOrderByCreatedAt();
+	@Query(value ="select distinct x from Template x join fetch x.templateItems where x.username = :username order by x.createdAt desc")
+	List<Template> findByUsernameOrderByCreatedAt(@Param("username") String username);
 	
-	List<Template> findByUsernameOrderByCreatedAt(String username);
+//	List<Template> findByUsernameOrderByCreatedAt(String username);
 }
